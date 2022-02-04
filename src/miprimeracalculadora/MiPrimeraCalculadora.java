36e0195a250955dec30989225b629fe3714d3b96
Juan Zorrilla <87153106+JuanZorrilla95@users.noreply.github.com>
2022-02-04 16:15:21 -0300

    Update MiPrimeraCalculadora.java

--- src/miprimeracalculadora/MiPrimeraCalculadora.java
+++ src/miprimeracalculadora/MiPrimeraCalculadora.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package miprimeracalculadora;
+import Paquete.Calculadora; //importar objeto
 
 
 /**
@@ -17,6 +18,9 @@ public class MiPrimeraCalculadora {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+       Calculadora c=new Calculadora(); 
+    c.setVisible(true);//estos 2 parrafos es para la creacion de la del objeto y su visualizacion
     }
     
 }
